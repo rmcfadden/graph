8c25fe3a50e4bd670e5b3c42476a9b3ff00e68c0
Ryan McFadden <ryanpmcfadden@gmail.com>
2018-08-05 13:35:24 +1000

    Views and layers

--- src/view.js
+++ src/view.js
@@ -4,28 +4,41 @@ import TopLayer from "./topLayer";
 export default class View {
   constructor(graph) {
     this.graph = graph;
+    this.layers = {};
 
     this.graph.canvasId = `canvas-${graph.id}`;
     const graphElement = document.getElementById(this.graph.id);
     graphElement.style.position = "relative";
-    graphElement.innerHTML = `<canvas id='${this.graph.canvasId}-top' style='position: absolute; left: 0; top: 0; z-index: 2;'></canvas>`
-      + `<canvas id='${this.graph.canvasId}' style='position: absolute; left: 0; top: 0; z-index: 1;'></canvas>`;
- 
-    this.layers = { 
-      default: new GraphLayer(this),
-      top: new TopLayer(this),
-    };
- 
+    graphElement.innerHTML = `<canvas id='canvas-${graph.id}-top' style='position: absolute; left: 0; top: 0; z-index: 2;'></canvas>`
+      + `<canvas id='canvas-${graph.id}' style='position: absolute; left: 0; top: 0; z-index: 1;'></canvas>`;
+
+    const graphLayer = new GraphLayer(this);
+    graphLayer.name = "default";
+    this.addLayer(graphLayer)
+
+    const topLayer = new TopLayer(this);
+    topLayer.name = "top";
+    this.addLayer(topLayer)
+    
     this.getSelectedLayer = () => this.layers.default; // default for now
 
-    window.onorientationchange = window.onresize;
     window.onresize = () => {
       this.adjustLayout();
       this.draw();
     };
-
+    window.onorientationchange = window.onresize;
+  
     this.adjustLayout();
     this.draw();
+  }
+
+  addLayer(layer) {
+    layer.index = this.layers.length;
+    this.layers[layer.name] = layer;
+  }
+
+  applyLayers () {
+
   }
 
   adjustLayout() {    
